ac7b080a7aa1106c0517a5e0a566c14ad3ba91de
Ozatooo <lukasz4044@o2.pl>
2022-10-02 00:06:38 +0200

    style

--- AddStone.php
+++ AddStone.php
@@ -5,7 +5,22 @@ if($_POST['check'] != "on")
     header("Location: ../Stone_Shop/main.html");
 }
 
-echo $_POST['name']
+$mysqli = new mysqli("https://www.db4free.net/phpMyAdmin/","stone_shop","KKFk321123!","stone_shop");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+// Perform query
+if ($result = $mysqli -> query("SELECT * FROM Stone_Data")) {
+  echo "Returned rows are: " . $result -> num_rows;
+  // Free result set
+  $result -> free_result();
+}
+
+$mysqli -> close();
 
 
 ?>
